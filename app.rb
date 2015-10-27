b622f998f85c800901b99a97108a1985e54e39bb
Sivan Patel <sivan.patel@googlemail.com>
2015-10-27 16:14:08 +0000

    Initial commit, sinatra and shotgun installed and basic routes set up

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+require 'shotgun'
+
+get '/' do
+  "Hello!!"
+end
+
+get '/secret' do
+  "This is a secret page"
+end
+
+get '/cat' do
+  "<div>
+  <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
